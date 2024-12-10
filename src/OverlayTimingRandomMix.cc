f3465c4689659c50c8548c1020bd9c4abe80165e
Federico Meloni <federico.meloni@cern.ch>
2024-12-10 11:50:27 +0100

    free up memory?

--- src/OverlayTimingRandomMix.cc
+++ src/OverlayTimingRandomMix.cc
@@ -407,6 +407,10 @@ namespace overlay
     {
       streamlog_out(DEBUG) << "Collection " << collection_names_in_evt->at(i) << " has now " << evt->getCollection(collection_names_in_evt->at(i))->getNumberOfElements() << " elements" << std::endl;
     }
+
+    // reset pointer
+    overlay_Evt = nullptr;
+
   }
 
   //------------------------------------------------------------------------------------------------------------------------------------------
